ba3a4dce6abc2f7aa68c1bd5388db10cfc4d6421
Selamawit T <165764715+tsgsgt98@users.noreply.github.com>
2024-06-03 21:59:33 -0700

    Add files via upload

--- week5/src/dailySpecials.java
+++ week5/src/dailySpecials.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class dailySpecials {
 
-    public static void main(String[]args ) {
+    public static void main(String[] args ) {
 
         String specials;
 
@@ -12,55 +12,64 @@ public class dailySpecials {
 
         specials = input.next();
 
-
+        input.close();
 //        in the world of coffee, I need a name of a coffee beverage and a price
 
 
         String coffee;
         double price;
 
- boolean saturday = specials.equals("Saturday");
-        boolean sunday = specials.equals("Sunday");
-
-        if(saturday || sunday) {
-           System.out.println("Please enter a weekday, not weekend");
-            specials = input.next();
-        }
+//        boolean saturday = specials.equals("Saturday");
+//        boolean sunday = specials.equals("Sunday");
+//
+//        if(saturday || sunday) {
+//           System.out.println("Please enter a weekday, not weekend");
+//            specials = input.next();
+//        }
 
 
         switch (specials) {
 
 //     my cases will be the day of the week
 
-            case "Monday" -> {
+            case "Monday":
                 coffee = "Latte";
                 price = 4.95;
+
 //         System.out.println(coffee);
 //         System.out.println(price);
 
                 System.out.println(specials + " 's coffee of the day is a  " + coffee + " and the price will be $" + price);
-            }
-            case "Tuesday" -> {
+                break;
+
+            case "Tuesday":
                 coffee = "Frapp";
                 price = 5.95;
                 System.out.println(specials + " 's coffee of thd day is a  " + coffee + " and the price will be $" + price);
-            }
-            case "Wednesday" -> {
+                break;
+
+            case "Wednesday":
                 coffee = "Cappucino";
                 price = 4.35;
                 System.out.println(specials + " 's coffee of thd day is a  " + coffee + " and the price will be $" + price);
-            }
-            case "Thursday" -> {
+                break;
+
+            case "Thursday":
                 coffee = "Regular Joe";
                 price = 2.95;
                 System.out.println(specials + " 's coffee of thd day is a  " + coffee + " and the price will be $" + price);
-            }
-            case "Friday" -> {
+                break;
+
+            case "Friday":
                 coffee = "Green Tea Latte";
                 price = 6.95;
                 System.out.println(specials + " 's coffee of thd day is a  " + coffee + " and the price will be $" + price);
+                break;
+
+                default:
+                System.out.println("Invalid input - Please check your spelling!!!");
             }
         }
-
     }
-}
+
+
